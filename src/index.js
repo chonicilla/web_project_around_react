4fb4ec3a6e7b42251f9c248cee3de5b2e1ff47e8
chonicilla <co.espinozas@gmail.com>
2026-08-31 10:53:18 -0400

    31 -08 update 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode, createElement } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./blocks/fonts.css";
+import "./fonts/fonts.css";
 import App from "./components/App.jsx";
 
 createRoot(document.getElementById("root")).render(
